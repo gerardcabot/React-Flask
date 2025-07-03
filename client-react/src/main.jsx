5a5b99201110349cb784d721a7554ca6c853723f
GerardCabot <gerard.cabot01@estudiant.upf.edu>
2025-07-04 00:21:24 +0200

    fix: Generate heatmaps in-memory for production

--- client-react/src/main.jsx
+++ client-react/src/main.jsx
@@ -1,7 +1,33 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Chart, BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend, ArcElement, PointElement, LineElement, Filler } from 'chart.js';
+
+// Registra aquí tots els components de Chart.js que la teva aplicació necessita
+Chart.register(
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  ArcElement,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
